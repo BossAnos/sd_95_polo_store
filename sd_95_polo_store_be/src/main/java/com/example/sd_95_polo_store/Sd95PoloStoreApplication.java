1ee45947bf0307ba78715bd68bf98893f4bc5129
BossAnos <hung220903@gmail.com>
2023-10-01 22:27:07 +0700

    Add new project

---
+++ sd_95_polo_store_be/src/main/java/com/example/sd_95_polo_store/Sd95PoloStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.sd_95_polo_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sd95PoloStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sd95PoloStoreApplication.class, args);
+    }
+
+}
---
+++ sd_95_polo_store_be/src/test/java/com/example/sd_95_polo_store/Sd95PoloStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sd_95_polo_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sd95PoloStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
